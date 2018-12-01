7a0a16efd1299cab9ef73615843a993da97f2e62
00162317 <00162317@uca.edu.sv>
2018-11-30 18:54:28 -0600

    Terminamos

--- configs/keys.js
+++ configs/keys.js
@@ -2,6 +2,7 @@
 module.exports = {
     mongodb:
     {
-        URI: 'mongodb://localhost:27017/drive' //Nombre de la base de datos.
+        //URI: 'mongodb://localhost:27017/drive' //Nombre de la base de datos.
+        URI: 'mongodb://Eduardo10:Eduardo10@ds123444.mlab.com:23444/drive'
     }
 }
